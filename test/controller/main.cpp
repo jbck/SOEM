711cc839132d3d04486beb8458bd38de8e48ea62
James Beck <jbeck@synapticon.com>
2018-05-22 12:53:49 +0200

    Disable auto-scale screen resolution
    This is necessary on the computer at work, but not at home. Dunno why.
    Maybe something to do with multi-monitor setup I have at work.

--- test/controller/main.cpp
+++ test/controller/main.cpp
@@ -5,6 +5,7 @@
 
 int main(int argc, char *argv[])
 {
+    qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "0");
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
